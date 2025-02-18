c1f2ff83ad0256c40b93c5983d4e07af02a3e786
Yoshikazu IMAI <imai@nu-soft.jp>
2025-02-18 18:24:47 +0900

    不要になったファイルを削除

--- HMIApp_Linux.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-python3 --version
-python3 src/VCIBSimuApp.py
--- HMIApp_Win.bat
+++
@@ -1,3 +0,0 @@
-CALL C:\Users\ObjectData\Anaconda3\Scripts\activate.bat C:\Users\ObjectData\Anaconda3
-CALL activate "hmi"
-python src/VCIBSimuApp.py
